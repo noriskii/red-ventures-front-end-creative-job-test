8157aec4077932184f2c728d423d44c1457b14e6
Noriskii <luckaugusto@gmail.com>
2018-04-28 19:40:11 +0000

    solving visual documentation

--- material-apoio/other-project/JavaScript Avançado III/aluraframe/client/js/app/polyfill/es6.js
+++
@@ -1,9 +0,0 @@
-if(!Array.prototype.includes) {
-    //Se não existir
-    console.log("Polyfill para Array.includes aplicado");
-    
-    Array.prototype.includes = function(elemento) {
-        return this.indexOf(elemento) != -1;    
-    };
-    
-}
--- material-apoio/other-project/JavaScript Avançado III/aluraframe/client/js/app/views/MensagemView.js
+++
@@ -1,9 +0,0 @@
-class MensagemView extends View {
-    
-    template(model) {
-        
-        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
-        
-    }
-    
-}
